7282fff93fa9cb7b32bdd24712c18fe55b7af189
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-06-25 16:07:10 +0100

    Installing files needed for project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDbvdzCE1GttS2OvPC90er9DruZAxmnlGo',
+    authDomain: 'angular-commerce-b7d1e.firebaseapp.com',
+    projectId: 'angular-commerce-b7d1e',
+    storageBucket: 'angular-commerce-b7d1e.appspot.com',
+    messagingSenderId: '654379842614',
+    appId: '1:654379842614:web:b4a31f0f4e0b4946ae2c97',
+    measurementId: 'G-FBP67JJ26S'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDbvdzCE1GttS2OvPC90er9DruZAxmnlGo',
+    authDomain: 'angular-commerce-b7d1e.firebaseapp.com',
+    projectId: 'angular-commerce-b7d1e',
+    storageBucket: 'angular-commerce-b7d1e.appspot.com',
+    messagingSenderId: '654379842614',
+    appId: '1:654379842614:web:b4a31f0f4e0b4946ae2c97',
+    measurementId: 'G-FBP67JJ26S'
+  }
 };
 
 /*
